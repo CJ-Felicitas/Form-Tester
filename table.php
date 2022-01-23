86d818d1d6136f685eb987cf057703b41d320829
CJ Felicitas <77721340+CJ-Felicitas@users.noreply.github.com>
2022-01-23 21:58:05 +0800

    add

---
+++ table.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<div class="container-fluid ">
+<h1>This is an example of a table</h1>
+<table class="table table-hover table-bordered">
+<thead>
+    <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Campus</th>
+    </tr>
+</thead>
+<tbody>
+<tr>
+    <td>Cedrick James</td>
+    <td>Felicitas</td>
+    <td>Obrero</td>
+</tr>
+
+<tr>
+    <td>Kiara Ysabelle</td>
+    <td>Hagunob</td>
+    <td>Obrero</td>
+</tr>
+<tr>
+    <td>Mark Jerome</td>
+    <td>Pondol</td>
+    <td>Obrero</td>
+</tr>
+<tr>
+    <td>Dorothy</td>
+    <td>Saliot</td>
+    <td>Tagum</td>
+</tr>
+</tbody>
+</table>
+</div>
+</body>
+
+</html>
