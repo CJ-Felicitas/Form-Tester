0b2c5c1feacf51849f3b8f6faed3382645ff41a8
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2022-02-01 21:22:27 +0800

    new tables and constraints for patient table

--- fehapo.sql
+++ fehapo.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2022 at 11:49 AM
+-- Generation Time: Feb 01, 2022 at 02:19 PM
 -- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.1
-
-DROP DATABASE IF EXISTS fehapo;
-CREATE DATABASE fehapo;
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,6 +24,26 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branch_id`, `branch_name`) VALUES
+(1, 'Obrero'),
+(2, 'Mintal'),
+(3, 'Tagum');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `employees`
 --
 
@@ -38,22 +55,65 @@ CREATE TABLE `employees` (
   `employee_password` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `employees`
+-- Table structure for table `patient`
 --
 
-INSERT INTO `employees` (`employee_id`, `employee_name`, `employee_email`, `branch`, `employee_password`) VALUES
-(1, 'Cedrick James Felicitas', 'cjbfelicitas@usep.edu.ph', 'Branch 1', 'secret');
+CREATE TABLE `patient` (
+  `patient_id` int(11) NOT NULL,
+  `patient_name` varchar(255) NOT NULL,
+  `patient_age` int(11) NOT NULL,
+  `branch_id` int(11) NOT NULL,
+  `date_admit` varchar(255) NOT NULL,
+  `description` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `employeeRole_id` int(11) NOT NULL,
+  `employeeRole_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`employeeRole_id`, `employeeRole_name`) VALUES
+(1, 'Doctor'),
+(2, 'Cashier'),
+(3, 'Nurse'),
+(4, 'Admin'),
+(5, 'Utility');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
 -- Indexes for table `employees`
 --
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`employee_id`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patient_id`),
+  ADD KEY `branch_id` (`branch_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -64,6 +124,16 @@ ALTER TABLE `employees`
 --
 ALTER TABLE `employees`
   MODIFY `employee_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `patient`
+--
+ALTER TABLE `patient`
+  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`branch_id`) REFERENCES `branch` (`branch_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
